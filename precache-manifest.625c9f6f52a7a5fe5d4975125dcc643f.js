b5e3882141488767a0966146f227378e6d86b4a1
Huw Tyler <huw@dragondrop.uk>
2024-11-21 15:39:57 +0000

    Updates

---
+++ precache-manifest.625c9f6f52a7a5fe5d4975125dcc643f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0107f86ca891f9cc4a2129c57e63d14",
+    "url": "/react_app/index.html"
+  },
+  {
+    "revision": "7e6d5ded925cc868e072",
+    "url": "/react_app/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "4ab975bd6c9bd0f545b0",
+    "url": "/react_app/static/js/2.7c0b4a00.chunk.js"
+  },
+  {
+    "revision": "7373c6e4eb6af1a9d439",
+    "url": "/react_app/static/js/3.ba8b7f13.chunk.js"
+  },
+  {
+    "revision": "7e6d5ded925cc868e072",
+    "url": "/react_app/static/js/main.e8e696a3.chunk.js"
+  },
+  {
+    "revision": "aa0ad8340a586886613b",
+    "url": "/react_app/static/js/runtime~main.f09c6f6a.js"
+  }
+]);
